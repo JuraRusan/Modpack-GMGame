c2f72e7d8bbe2c88114348877c63da1a9d6ced6f
JuraRusan <rusanjura@gmail.com>
2025-01-10 00:13:52 +0200

    Add infinity cell lava

--- kubejs/startup_scripts/Item.js
+++ kubejs/startup_scripts/Item.js
@@ -1,3 +1,8 @@
 StartupEvents.registry("item", (e) => {
   e.create("kubejs:universal_press").displayName("Universal Press");
+
+  e.create("infinity_lava_cell", "custom_infinity_cell")
+    .texture("kubejs:item/infinity_lava_cell")
+    .fluidType("minecraft:lava")
+    .cellModel("kubejs:block/drive/infinity_lava_cell");
 });
