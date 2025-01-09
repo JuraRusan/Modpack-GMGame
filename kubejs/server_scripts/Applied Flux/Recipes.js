23037af3aaa656f835b21fd0447f1be0a34b7512
JuraRusan <rusanjura@gmail.com>
2025-01-09 20:08:21 +0200

    megacells & appflux remove recipes

---
+++ kubejs/server_scripts/Applied Flux/Recipes.js
@@ -0,0 +1,4 @@
+ServerEvents.recipes(e => {
+  e.remove({id: 'appflux:inscriber/crush_diamond'})
+  e.remove({id: 'appflux:inscriber/crush_emerald'})
+});
---
+++ kubejs/server_scripts/Mega Cells/Recipes.js
@@ -0,0 +1,6 @@
+ServerEvents.recipes(e => {
+  e.remove({output: 'megacells:mega_interface'})
+  e.remove({output: 'megacells:cable_mega_interface'})
+  e.remove({output: 'megacells:mega_pattern_provider'})
+  e.remove({output: 'megacells:cable_mega_pattern_provider'})
+});
