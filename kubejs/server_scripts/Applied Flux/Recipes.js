1ae56c91fea55a2ed025b7962fcff360047ee6a6
JuraRusan <rusanjura@gmail.com>
2025-01-10 00:14:34 +0200

    remove item to move

--- kubejs/server_scripts/Applied Flux/Recipes.js
+++
@@ -1,4 +0,0 @@
-ServerEvents.recipes((e) => {
-  e.remove({ id: "appflux:inscriber/crush_diamond" });
-  e.remove({ id: "appflux:inscriber/crush_emerald" });
-});
--- kubejs/server_scripts/Mega Cells/Recipes.js
+++
@@ -1,6 +0,0 @@
-ServerEvents.recipes((e) => {
-  e.remove({ output: "megacells:mega_interface" });
-  e.remove({ output: "megacells:cable_mega_interface" });
-  e.remove({ output: "megacells:mega_pattern_provider" });
-  e.remove({ output: "megacells:cable_mega_pattern_provider" });
-});
