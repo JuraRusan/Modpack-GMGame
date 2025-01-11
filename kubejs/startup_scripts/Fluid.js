e2337afdbf076f09a720ee71229b286826433ad3
JuraRusan <rusanjura@gmail.com>
2025-01-11 15:10:15 +0200

    add 2 fluid

---
+++ kubejs/startup_scripts/Fluid.js
@@ -0,0 +1,4 @@
+StartupEvents.registry("fluid", (e) => {
+  e.create("kubejs:molten_iridium", "thick").tint("white_dye").displayName("Molten Iridium").noBucket().noBlock();
+  e.create("kubejs:molten_neutronium", "thick").tint("dark_gray").displayName("Molten Neutronium").noBucket().noBlock();
+});
