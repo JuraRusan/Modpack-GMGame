9d150e1d99e5e9293ce2ad8ab7fb8509392b80de
JuraRusan <rusanjura@gmail.com>
2025-01-12 00:15:22 +0200

    new shape machines = LARGE_ADVANCED_STEAM_TURBINE

---
+++ kubejs/startup_scripts/Machines/Large_advanced_steam_turbine.js
@@ -0,0 +1,40 @@
+MIMachineEvents.registerMachines((e) => {
+  // A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+
+  const hatchFluid = e.hatchOf("fluid_input");
+  const hatchEnergy = e.hatchOf("energy_output");
+  const A_key = e.memberOfBlock("modern_industrialization:solid_titanium_machine_casing");
+  const B_key = e.memberOfBlock("modern_industrialization:titanium_machine_casing_pipe");
+
+  const LargeAdvancedSteamTurbineShape = e
+    .layeredShape("solid_titanium_machine_casing", [
+      ["AAA", "AYA", "AAA"],
+      ["BBB", "BBB", "BBB"],
+      ["BBB", "BBB", "BBB"],
+      ["ZZZ", "Z#Z", "ZZZ"],
+    ])
+    .key("Z", A_key, hatchFluid)
+    .key("Y", A_key, hatchEnergy)
+    .key("A", A_key, e.noHatch())
+    .key("B", B_key, e.noHatch())
+    .build();
+
+  e.simpleGeneratorMultiBlock(
+    "Large advanced steam turbine", // English name
+    "large_advanced_steam_turbine", // Internal name
+    LargeAdvancedSteamTurbineShape, // Multiblock shape
+    256000, // Maximum energy generation rate (eu/tick)
+    (builder) => {
+      // The builder (same as for a single block generator)
+      builder.fluid("modern_industrialization:steam", 1);
+      builder.fluid("modern_industrialization:heavy_waster_steam", 1);
+      builder.fluid("modern_industrialization:high_pressure_steam", 8);
+      builder.fluid("modern_industrialization:high_pressure_heavy_water_steam", 8);
+    },
+    "solid_titanium_machine_casing", // Casing of the controller
+    "large_advanced_steam_turbine", // Overlay folder
+    true, // Front overlay?
+    false, // Top overlay?
+    false // Side overlay?
+  );
+});
